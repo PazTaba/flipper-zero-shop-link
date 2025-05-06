305d48a7bd06024ad07a7cd8fd42b7fdeb09321a
Paz <paztabachnik@gmail.com>
2025-05-06 10:26:46 +0300

    add

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
+import App from "./App";
+import SEOWrapper from "./components/SEOWrapper";
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <SEOWrapper>
+    <App />
+  </SEOWrapper>
+);
